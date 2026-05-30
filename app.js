c3f660168c1da456a30bf6032e84a6f7bd1cb648
lakshay1403 <rudrakshg16@gmail.com>
2026-05-30 15:18:35 +0530

    passport feature added for session based login for the users and middleware

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+require('dotenv').config();
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const {MongoStore} = require("connect-mongo")
+console.log(typeof MongoStore.create);
 const userRoute = require("./routes/authRoutes");
-require('dotenv').config();
-
+const passportConfig = require('./config/passport');
+const passport = require('passport');
+const session = require("express-session");
 //port
 const PORT = process.env.PORT || 3000
 //middlewares: passing form data
 app.use(express.urlencoded({extended: true}));
+
+//session middleware
+app.use(session({
+    secret: "Keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({mongoUrl: process.env.MONGODB_URL}),
+   })
+);
+//passport
+passportConfig(passport);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //EJS
 app.set("view engine", "ejs");
